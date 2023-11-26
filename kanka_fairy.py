836f1ae299f473ba89c63047b8a1a706959ebf54
Jonathan Skirtun <53845414+JSGreg@users.noreply.github.com>
2023-11-26 18:49:20 +1100

    Set up slash commands
    Set up slash commands
    Changed from client to bot

--- kanka_fairy.py
+++ kanka_fairy.py
@@ -1,6 +1,10 @@
+from ast import arg
 from html import entities
+from http import server
 from logging import info
 import discord
+from discord import app_commands
+from discord.ext import commands
 import os
 import requests
 import json
@@ -14,33 +18,64 @@ import json
 # Set variables earlier 
 
 
-REQUEST_PATH = "https://kanka.io/api/1.0/"
+REQUEST_PATH = "https://api.kanka.io/1.0/"
 MSG_ENTITY_NOT_FOUND = "Entity not found."
 MSG_REQUIRE_TOKEN = "Please provide token"
 MSG_NO_MATCHING_CAMPAIGN = "Found no matching campaign. \nPlease check that server matches Kanka campaign"
 
-client = discord.Client(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-@client.event
+# client = discord.Client(intents=discord.Intents.all())
 
+@bot.event
 async def on_ready():
-    print('Kanka Fairy is logged on'.format(client))
+    print('Kanka Fairy is logged on'.format(bot))
+    try: 
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
-@client.event
-async def on_message (message:discord.Message):
-    if (message.author == client.user):
-        return
-    if (message.content.startswith('$hello')):
-        await message.channel.send('Hello')
-    # if (message.author.roles == message.author.roles):
-    
-    #
-    server = get_campaignID_by_name(message)
+# Slash command intergration
+# TODO
+@bot.tree.command(name = "hello")
+async def hello (interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hi {interaction.user.mention}!")
 
-    # print(kanka_info ["data"]["name"])
-    print("this is the server:" + message.guild.name)
+# TODO
+@bot.tree.command(name = "map")
+@app_commands.describe(map_name = "Map name")
+async def kmap (interaction: discord.Interaction, map_name:str):
+    return
 
-    await message.channel.send(server["id"])
+# TODO
+@bot.tree.command(name = "location")
+@app_commands.describe(loc_name = "Location name")
+async def location (interaction: discord.Interaction, loc_name: str):
+    return
+
+# TODO
+@bot.tree.command(name = "journal")
+@app_commands.describe(journal_name = "Journal name")
+async def journal (interaction: discord.Interaction, journal_name: str):
+    return
+
+# TODO
+@bot.tree.command(name = "note")
+@app_commands.describe(note_name = "Note name")
+async def note (interaction: discord.Interaction, note_name: str):
+    return
+
+# TODO
+@bot.tree.command(name = "character")
+@app_commands.describe(character_name = "Character name")
+async def character (interaction: discord.Interaction, character_name:str):
+    serverID = get_campaignID_by_name(interaction)
+    char_name = api_call(interaction.user.name, serverID)
+
+    await interaction.response.send_message(char_name)
+    return
+
     # ./campaigns endpoint dict syntax "kanka_info[data][campaign number][information]"
     # ./campaigns/{id} dict syntax "kanka_info[data][name]"
     # Can use this to verify which campaign player is in by server
@@ -51,15 +86,15 @@ async def on_message (message:discord.Message):
 # Get Kanka server by name 
 # return campaignID
 
-def get_campaignID_by_name(message:discord.Message):
-    kanka_info = api_call(os.getenv(message.author.name + '_TOKEN'))
+def get_campaignID_by_name(interaction:discord.Interaction):
+    kanka_info = api_call(os.getenv(interaction.user.name + '_TOKEN'))
     
     # Loops thorugh dict and matches server to campaign 
     # returns campaignID
     for x in kanka_info["data"]:
-        if (message.guild.name == x["name"]):
-            print ("this is the kanka campaign:" + x["name"])
-            return x
+        if (interaction.guild.name == x["name"]):
+            # print ("this is the kanka campaign:" + x["name"])
+            return x["id"]
 
     return MSG_NO_MATCHING_CAMPAIGN
 
@@ -67,7 +102,7 @@ def get_campaignID_by_name(message:discord.Message):
 # May need to make a seperate api call for entities and characters
 # ^  Don't forget that all endpoints documented here need to be prefixed with "1.0/campaigns/{campaign.id}/."
 def api_call (user_name, campaign_id = "", entity = ""):
-    url = REQUEST_PATH + "campaigns/" + campaign_id + entity # here we add variable to specific sections? can add campaign id to the end of url to specify among every campaign stored on kanka
+    url = REQUEST_PATH + "campaigns/" + str(campaign_id) + entity # here we add variable to specific sections? can add campaign id to the end of url to specify among every campaign stored on kanka
 
     headers = {
         'Content-Type': 'application/json',
@@ -80,4 +115,4 @@ def api_call (user_name, campaign_id = "", entity = ""):
 
     return response 
 
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
